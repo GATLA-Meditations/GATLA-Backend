6f81b8384cf44cec8a635ed00adff421acacc97a
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2024-05-07 08:55:06 -0300

    feat: questionnaire seed

---
+++ prisma/seed/seed.ts
@@ -0,0 +1,60 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+questionnaireSeed()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
+
+async function questionnaireSeed() {
+  await prisma.questionnaire.upsert({
+    where: { id: 'questionnaireId' },
+    update: {},
+    create: {
+      id: 'questionnaireId',
+      name: 'Escala de satisfacción con la vida',
+      questions: {
+        createMany: {
+          data: [
+            {
+              id: 'questionId1',
+              type: 'NUMERIC',
+              name: 'En muchos aspectos, mi vida se acerca a mi ideal',
+              metadata: '{ min: 1, max: 7 }',
+            },
+            {
+              id: 'questionId2',
+              type: 'NUMERIC',
+              name: 'Mis condiciones de vida son excelentes',
+              metadata: '{ min: 1, max: 7 }',
+            },
+            {
+              id: 'questionId3',
+              type: 'NUMERIC',
+              name: 'Estoy satisfecho/a con mi vida',
+              metadata: '{ min: 1, max: 7 }',
+            },
+            {
+              id: 'questionId4',
+              type: 'NUMERIC',
+              name: 'Hasta ahora, he conseguido las cosas importantes que quiero en la vida',
+              metadata: '{ min: 1, max: 7 }',
+            },
+            {
+              id: 'questionId5',
+              type: 'NUMERIC',
+              name: 'En muchos aspectos, mi vida se acerca a mi ideal',
+              metadata: '{ min: 1, max: 7 }',
+            },
+          ],
+        },
+      },
+    },
+  });
+}
