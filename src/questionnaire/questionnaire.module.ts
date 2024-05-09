d48f00799575db17beab797068490aec63ffbcdc
SantiGrimoldi <Santiago.grimoldi@ing.austral.edu.ar>
2024-05-09 11:18:55 -0300

    exported questionnaire.repository.ts to be able to look for answers from the submission.module.ts

--- src/questionnaire/questionnaire.module.ts
+++ src/questionnaire/questionnaire.module.ts
@@ -7,5 +7,6 @@ import { PrismaService } from '../prisma.service';
 @Module({
   controllers: [QuestionnaireController],
   providers: [PrismaService, QuestionnaireRepository, QuestionnaireService],
+  exports: [QuestionnaireRepository],
 })
 export class QuestionnaireModule {}
