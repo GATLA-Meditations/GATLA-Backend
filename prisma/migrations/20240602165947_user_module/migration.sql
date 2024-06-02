551b806d9fbdb2717f25b8a8237c8a08595e6e8c
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2024-06-02 14:36:05 -0300

    added user_module

---
+++ prisma/migrations/20240602165947_user_module/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "UserModule" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "minutesSpent" INTEGER[],
+    "userId" TEXT NOT NULL,
+    "moduleId" TEXT NOT NULL,
+
+    CONSTRAINT "UserModule_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "UserModule" ADD CONSTRAINT "UserModule_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserModule" ADD CONSTRAINT "UserModule_moduleId_fkey" FOREIGN KEY ("moduleId") REFERENCES "Module"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,22 @@ import { AuthService } from './auth.service';
 import { AuthRepository } from './auth.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaService } from '../prisma.service';
+import { ConfigModule } from '@nestjs/config';
+import { configuration } from '../../config/configuration';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  imports: [JwtModule],
-  providers: [AuthService, AuthRepository, PrismaService],
+  imports: [
+    JwtModule.register({
+      global: true,
+      signOptions: { expiresIn: '1d' },
+    }),
+    ConfigModule.forRoot({
+      envFilePath: `${process.cwd()}/.env`,
+      load: [configuration],
+    }),
+  ],
+  providers: [AuthService, AuthRepository, PrismaService, JwtStrategy],
 })
 export class AuthModule {}
