fcb53db54513e3758f5973e4377a2f57fc0dfa29
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2024-06-03 11:23:53 -0300

    cors config

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder().setTitle('Gatla').setDescription('Gatla endpoints').setVersion('1.0').addTag('endpoints').build();
   const document = SwaggerModule.createDocument(app, config);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   SwaggerModule.setup('api', app, document);
   await app.listen(3001);
 }
