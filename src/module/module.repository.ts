dff0478d5eecb9282b15faf014790bf78bd69488
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2024-06-08 13:28:39 -0300

    fix: simple activity dtos

--- src/module/module.repository.ts
+++ src/module/module.repository.ts
@@ -27,7 +27,11 @@ export class ModuleRepository {
       include: {
         module: {
           include: {
-            activities: true,
+            activities: {
+              include: {
+                activity: true,
+              },
+            },
           },
         },
         minutesSpent: true,
