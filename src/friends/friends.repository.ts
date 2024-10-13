423aa9f2ae52d4f206dbcc282f7b8b06d5abed36
SantiGrimoldi <Santiago.grimoldi@ing.austral.edu.ar>
2024-10-13 11:00:23 -0300

    Feat: created baseline for friends integration

---
+++ src/friends/friends.repository.ts
@@ -0,0 +1,5 @@
+import { PrismaService } from "src/prisma.service";
+
+export default class FriendsRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+}
---
+++ src/friends/friends.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@nestjs/common';
+import FriendsRepository from './friends.repository';
+
+@Injectable()
+export class FriendsService {
+    constructor(private readonly repository: FriendsRepository) {}
+}
