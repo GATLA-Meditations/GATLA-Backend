b193b34cea840648c580698aeff0fd3ba51b75a5
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2024-10-14 19:02:58 -0300

    added Mailer Module from nestjs

---
+++ src/mail/mail.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { MailService } from './mail.service';
+import path from 'path';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+
+@Module({
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: () => ({
+        transport: {
+          host: 'smtp.hostinger.com',
+          secure: true,
+          tls: {
+            ciphers: 'SSLv3',
+            rejectUnauthorized: false,
+          },
+          requireTLS: true,
+          port: 465,
+          auth: {
+            user: process.env.EMAIL,
+            pass: process.env.EMAIL_PASSWORD,
+          },
+        },
+        template: {
+          dir: path.join(process.cwd(), '/src/mail/templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
+  ],
+  providers: [MailService],
+})
+export class MailModule {}
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,30 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import * as nodemailer from 'nodemailer';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class MailService {
-  private transporter;
-
-  constructor() {
-    this.transporter = nodemailer.createTransport({
-      host: 'smtp.hostinger.com',
-      secure: true,
-      tls: {
-        ciphers: 'SSLv3',
-        rejectUnauthorized: false,
-      },
-      requireTLS: true,
-      port: 465,
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
-  }
+  constructor(private readonly mailerService: MailerService) {}
 
   async sendMail(to: string, subject: string, text: string, html?: string): Promise<void> {
     try {
-      await this.transporter.sendMail({
+      await this.mailerService.sendMail({
         from: 'Renacentia eugenio@renacentia.org',
         to,
         subject,
